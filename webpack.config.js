8c0c0a280413efc0718e688e222192dae2f69ecc
nadongjun <aa9814526@gmail.com>
2019-07-01 14:18:43 +0900

    Intialize React Project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+'use strict'
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: ['./src/main.js']
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, './src'),
+            loaders: 'babel-loader'
+        }]
+    },
+    plugins: [],
+    devServer: {
+        contentBase: './public',//public 폴더 안  index 파일
+        host: 'localhost',
+        port:8080
+    }
+}
